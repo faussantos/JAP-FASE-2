85bdf9b1d5a6cec3cc8089fd569c5370deb937fe
Faustino Santos <faus.santos@hotmail.com>
2023-09-02 21:19:52 -0300

    Trabajo finalizado

--- Desarrollo Web/Semana 9/Tarea individual/Trabajando con Arrays/js/script.js
+++ Desarrollo Web/Semana 9/Tarea individual/Trabajando con Arrays/js/script.js
@@ -25,18 +25,24 @@ const strangeArray = [
 ];
 
 // Función que recibe por parámetro un array y muestra sus elementos en pantalla
-function showList(array) {
+function showStringListAlphabet(array) {
   const container = document.getElementById("list");
   container.innerHTML = "";
   // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-  array.forEach((element) => {
+  const stringFilter = array.filter((element) => typeof element === "string").sort(function(a, b) {
+    if (a.toLowerCase()<b.toLowerCase()){return -1;}
+    if(a.toLowerCase() > b.toLowerCase()){return 1;}
+      return 0;
+  });
+  stringFilter.forEach((stringFilter) => {
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(element));
+    li.appendChild(document.createTextNode(stringFilter));
     container.appendChild(li);
   });
 }
 
-document.addEventListener("DOMContentLoaded", (e) => {
+document.addEventListener("DOMContentLoaded", () => {
   // Escribe tu solución aquí
   // Sugerencia de cómo mostrar el array => showList(strangeArray);
+  showStringListAlphabet(strangeArray)
 });
